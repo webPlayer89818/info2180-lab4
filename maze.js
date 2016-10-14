44fd45b05948878a9d63281d088e5a31f10baa3f
Micah McHayle <kem.jam876ix@gmail.com>
2016-10-14 04:41:28 +0000

    #4 complete, feeling accomplished

--- maze.js
+++ maze.js
@@ -3,48 +3,64 @@
 window.onload = function(){
     var gameStatus = true;
     
+    
+    //#1
+    //changes boundary of upper left wall only
+	var wall1 = document.getElementById('boundary1');
+	wall1.addEventListener('mouseover', function(){
+		this.setAttribute('class','boundary youlose');
+		
+	});
+
+
+    //#2
+    //changes boundary of all walls
+    var boundaries = document.querySelectorAll('.boundary');
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].onmouseover = overBoundary;
+        
+        
+    }
+    
+    //function alerts user to loss and changes boundaries of all walls
+    function overBoundary(){
+        gameStatus = false;
+        alert('You Lose!');
+        for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].setAttribute('class','boundary youlose');
+        
+        
+    }
+    }
+    
     //#3
+    //Alerts user that they win if the hover over "E" without touching walls
     var mazeEnd = document.getElementById('end');
     mazeEnd.addEventListener('mouseover', function(){
 		if(gameStatus === true){
 		alert('You WIN!');
 		}
 		else{
-		    alert('HaHa Nice try, you already lost');
+		    alert('HaHa Nice Try!, You Already Lost!');
 		}
 	
 	});
     
-    //#1
-    //code below changes boundary of upper left wall with id boundary1 upon moseover
-	var wall1 = document.getElementById('boundary1');
-	wall1.addEventListener('mouseover', function(){
-		this.setAttribute('class','boundary youlose');
-		//alert('you lose');
-		//gameStatus = 'inactive';
-	});
-
-    //#2
-    //code below changes boundary of all walls with class boundary upon moseover
-    var boundaries = document.querySelectorAll('.boundary');
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = overBoundary;
-        
-        //gameStatus = 'inactive';
-    };
     
-    function overBoundary(){
-        gameStatus = false;
-        alert('you lose');
+    //#4
+    //resets game by pressing "S", prompts user game has been reset
+    var startButton = document.getElementById("start");
+    startButton.addEventListener('click', function(){
+        alert('Game reset!');
+        gameStatus = true;
         for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].setAttribute('class','boundary youlose');
-        
-        
-    }
-    
+        boundaries[i].setAttribute('class','boundary');
+        }
+    });
     
     
     
 };
 
-}
+
+
